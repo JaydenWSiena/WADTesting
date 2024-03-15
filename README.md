f349cb8238c51f0a4f9a5adab7635b4c6ac09069
JaydenWSiena <jm12wojc@siena.edu>
2024-03-15 11:02:46 -0400

    start docs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WADTesting
+
+1. First item
+2. Second Item
+3. Third Item
+    1. Indented item
+    2. Indented item
+4. Fourth Item
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/math/circle/:radius', function (req,res) {
     if (!req.params.radius) {
         res.type("json")
             .status(400) // Bad request
-            .send({status:"error",error:"Radius has not been defined."});
+            .send({error:"Radius has not been defined."});
         return;
     }
     let radius = parseFloat(req.params.radius);
@@ -16,14 +16,14 @@ app.get('/math/circle/:radius', function (req,res) {
     let area = Math.PI*Math.pow(radius,2);
     res.type("json")
         .status(200) // OK
-        .send({status:"ok",circumference, area});
+        .send({circumference, area});
 });
 
 app.get('/math/rectangle/:length/:width', function (req,res) {
     if (!req.params.length || !req.params.width) {
         res.type("json")
             .status(400) // Bad request
-            .send({status:"error",error:"Length and width must be defined."});
+            .send({error:"Length and width must be defined."});
         return;
     }
     let length = parseFloat(req.params.length);
@@ -32,28 +32,34 @@ app.get('/math/rectangle/:length/:width', function (req,res) {
     let area = length*width;
     res.type("json")
         .status(200) // OK
-        .send({status:"ok", area, perimeter});
+        .send({area, perimeter});
 });
 
 app.get('/math/power/:base/:exponent', function (req,res) {
-    if (!req.params.base || !req.params.exponent) {
+    try {
+        if (!req.params.base || !req.params.exponent) {
+            res.type("json")
+                .status(400) // Bad request
+                .send({error:"Invalid base or exponent. Please provide numeric values."});
+            return;
+        }
+        if (req.query && req.query.root && req.query.root == "true") {
+            let base = parseFloat(req.params.base);
+            let exponent = parseFloat(req.params.exponent);
+            res.type("json")
+                .status(200) // OK
+                .send({result: Math.pow(base, exponent), root: Math.pow(base, (1 / 2))});
+        } else {
+            let base = parseFloat(req.params.base);
+            let exponent = parseFloat(req.params.exponent);
+            res.type("json")
+                .status(200) // OK
+                .send({result: Math.pow(base, exponent)});
+        }
+    } catch (err) {
         res.type("json")
-            .status(400) // Bad request
-            .send({status:"error",error:"Base and exponent must be defined."});
-        return;
-    }
-    if (req.query && req.query.root && req.query.root == "true") {
-        let base = parseFloat(req.params.base);
-        let exponent = parseFloat(req.params.exponent);
-        res.type("json")
-            .status(200) // OK
-            .send({status:"ok", result: Math.pow(base, exponent), root: Math.pow(base, (1 / exponent))}); // Takes inverse because that makes it a root
-    } else {
-        let base = parseFloat(req.params.base);
-        let exponent = parseFloat(req.params.exponent);
-        res.type("json")
-            .status(200) // OK
-            .send({status:"ok", result: Math.pow(base, exponent)});
+            .status(500) // OK
+            .send({error:"Something went wrong; please try again."});
     }
 });
 
